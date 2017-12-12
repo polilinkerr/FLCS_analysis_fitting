a2b3e2b34933ef0cc6edc42af60b167f2556cd56
polilinkerr <polilinker@yahoo.com>
2017-12-12 23:56:25 +0100

    Upload starego kodu
    Wczytuje krzywe, Tworzy obiekt klasy krzywa. Jej atrybuty fo parametry
    fitu oraz listy do tworzenia wykresow

---
+++ PodFLCS/Main.py
@@ -0,0 +1,87 @@
+
+import os
+from SingleCurveFromFLCS import SingleCurveFLCS_TripletModelOneSpecies
+from SingleCurveFromFLCS import GlobalStatisticParameters
+import numpy as np
+path  = os.getcwd()
+
+
+
+def readFiles(fileName):
+    pathToFIle = os.path.join(path,fileName)
+    k = open(pathToFIle)
+    lines = k.readlines()
+    #print lines[2].split("=")[1]
+    #print float(lines[2].split("=")[1].replace(" ","").replace(",","."))
+    return lines
+    pass
+
+
+def globalStatistic(LISTA_KRZYWYCH):
+
+
+    pass
+
+
+def letsdoSomeWork(fileName):
+    linesFormFile = readFiles(fileName)
+    LISTA_KRZYWYCH = []
+    if linesFormFile[7].split(" ")[0][:2] == "D1":
+        """Model Triplet State bez extende , bez alfy"""
+        #print "Model Triplet State bez extende , bez alfy"
+        Model1 = SingleCurveFLCS_TripletModelOneSpecies(fileName,linesFormFile)
+
+
+    elif linesFormFile[7].split(" ")[0][:2] == "a1":
+         """Model triplet extended  z alfa"""
+    else:
+        pass
+    try:
+        return  Model1
+    except:
+        print "Error z plikiem ", fileName
+        return None
+
+def printStatistic(GlobalStatisticParam):
+    print "D1: " , np.mean(GlobalStatisticParam.D1List), "+/-", np.std(GlobalStatisticParam.D1List), "Liczba Elementow ", len(GlobalStatisticParam.D1List)
+    print "tau1: ", np.mean(GlobalStatisticParam.t1List), "+/-", np.std(
+        GlobalStatisticParam.t1List), "Liczba Elementow ", len(GlobalStatisticParam.t1List)
+    print "<N>: ", np.mean(GlobalStatisticParam.NList), "+/-", np.std(
+        GlobalStatisticParam.NList), "Liczba Elementow ", len(GlobalStatisticParam.NList)
+    print "<C>: ", np.mean(GlobalStatisticParam.CList), "+/-", np.std(
+        GlobalStatisticParam.CList), "Liczba Elementow ", len(GlobalStatisticParam.CList)
+    pass
+
+
+def CheckIfModelIsGood(Model1):
+    flag = False
+
+    if Model1.D1 < 50. and  Model1.D1>10.:
+        flag = True
+    return flag
+def main():
+    files = [f for f in os.listdir(path) if (os.path.isfile(f) & f.endswith("dat"))]
+    print "Total Files Number", len(files)
+    LISTA_KRZYWYCH = []
+                    for f in files:
+                        Model1 = letsdoSomeWork(f)
+                        if Model1 is None: break
+
+                        CheckFlag = CheckIfModelIsGood(Model1)  ####### SPRAWDZA CZY DANA KRZYWA SIE NADAJE
+                        if CheckFlag:
+                            LISTA_KRZYWYCH.append(Model1)
+                            del Model1
+                        else:
+                            print Model1.fileName, "was deleted due to D1: ",Model1.D1
+                    """NOWY KOD"""
+    #print "LiczbaOgnisk", len(LISTA_KRZYWYCH)
+    print "SSS", len(LISTA_KRZYWYCH)
+    GlobalStatisticParam = GlobalStatisticParameters(LISTA_KRZYWYCH)
+
+    printStatistic(GlobalStatisticParam)
+
+
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ PodFLCS/SingleCurveFromFLCS.py
@@ -0,0 +1,121 @@
+
+
+class SingleCurveFromFLCS(object):
+
+    def __init__(self,name,filelines):
+        self.fileName = name
+        self.VEff = self.parsowanieParametrowFitu("VEff[fl]",filelines)
+        self.Z0 =  self.parsowanieParametrowFitu("z0",filelines)
+        self.W0 = self.parsowanieParametrowFitu("w0", filelines)
+        self.r1 = self.parsowanieParametrowFitu("r1", filelines)
+        self.t1 = self.parsowanieParametrowFitu("t1", filelines)
+        self.D1 = self.parsowanieParametrowFitu("D1", filelines)
+        self.N = self.parsowanieParametrowFitu("N", filelines)
+        self.C = self.parsowanieParametrowFitu("<C>", filelines)
+        self.kappa = self.parsowanieParametrowFitu("k=", filelines)
+        self.T = self.parsowanieParametrowFitu("T =", filelines)
+        self.tT = self.parsowanieParametrowFitu("tT[ms]", filelines)
+
+
+        self.time_List = self.parsowanieListForCurves(filelines,0)
+        self.Ex_Corr_List = self.parsowanieListForCurves(filelines,1)
+        self.Fit_Corr_List = self.parsowanieListForCurves(filelines,2)
+        self.Residuals_List = self.parsowanieListForCurves(filelines,3)
+
+    def parsowanieParametrowFitu(self, key, linesFromFile):
+        for i in linesFromFile:
+            if key in i.split(" ")[0]:
+
+                value = i.split(" = ")[1].replace(" ","").replace(",",".")
+
+
+                if "+-" in value:
+                    value = value.split("+-")[0]
+
+                break
+        try:
+            return float(value)
+        except:
+            return None
+
+
+    def odKtorejLinijkiSaDane(self,filelines):
+        pattern = None
+        for i in filelines:
+            if "Ex. Corr. Fit Corr. Resid" in i:
+                pattern = i
+                break
+        try:
+            indexStart = filelines.index(pattern)
+        except:
+            print "KB Error"
+        return indexStart
+
+    def parsowanieListForCurves(self, filelines,column):
+        indexStart = self.odKtorejLinijkiSaDane(filelines)
+        tempList = []
+        for line in filelines[indexStart+1:]:
+            values = line.split()
+            try:
+                tempList.append(float(values[column].replace(",",".")))
+            except IndexError:
+                tempList.append(0)
+        return tempList
+
+
+
+class SingleCurveFLCS_TripletModelOneSpecies(SingleCurveFromFLCS):
+
+    def __init__(self,nameFile,lines):
+
+        super(SingleCurveFLCS_TripletModelOneSpecies,self).__init__(nameFile,lines)
+
+
+
+
+
+class GlobalStatisticParameters():
+
+    def __init__(self, ListaKrzywych):
+        self.VEffList =  []
+        self.Z0List = []
+        self.W0List = []
+        self.r1List = []
+        self.t1List = []
+        self.D1List = []
+        self.NList = []
+        self.CList = []
+        self.kappaList = []
+        self.TList = []
+        self.tTList = []
+        self.Obliczenia(ListaKrzywych)
+
+        self.time_List_Global = []
+        self.Ex_Corr_List_Global = []
+        self.Fit_Corr_List_Global = []
+        self.Residuals_List_Global = []
+        self.Obliczanie2(ListaKrzywych)
+
+    def Obliczenia(self, ListaKrzywych):
+        for krzywa in ListaKrzywych:
+            self.VEffList.append(krzywa.VEff)
+            self.Z0List.append(krzywa.Z0)
+            self.W0List.append(krzywa.W0)
+            self.r1List.append(krzywa.r1)
+            self.t1List.append(krzywa.t1)
+            self.D1List.append(krzywa.D1)
+            self.NList.append(krzywa.N)
+            self.CList.append(krzywa.C)
+            self.kappaList.append(krzywa.C)
+            self.TList.append(krzywa.T)
+            self.tTList.append(krzywa.tT)
+
+        pass
+
+    def Obliczanie2(self, ListaKrzywych):
+        for krzywa in ListaKrzywych:
+            self.time_List_Global.append(krzywa.time_List)
+            self.Ex_Corr_List_Global.append(krzywa.Ex_Corr_List)
+            self.Fit_Corr_List_Global.append(krzywa.Fit_Corr_List)
+            self.Residuals_List_Global.append(krzywa.Residuals_List)
+        pass
